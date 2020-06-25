49311f07bcf9f197e50848da5bb8bd23533434dd
seednode <git@seedno.de>
2020-06-25 15:32:08 -0400

    No longer push image to registry

--- build.sh
+++ build.sh
@@ -32,7 +32,7 @@ docker build --build-arg version="$version" -t docker.seedno.de/seednode/nginx-p
 rm "$basedir"/image/nginx
 
 # log in to docker registry
-pass show docker-credential-helpers/docker-pass-initialized-check && docker login docker.seedno.de
+#pass show docker-credential-helpers/docker-pass-initialized-check && docker login docker.seedno.de
 
 # push the image to registry
-docker push docker.seedno.de/seednode/nginx-php:"$version"
+#docker push docker.seedno.de/seednode/nginx-php:"$version"
